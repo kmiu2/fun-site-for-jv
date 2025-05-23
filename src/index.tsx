cf6d1517417d9ba9961dfd196ab5055c1345183b
kmiu2 <kennymiu@rogers.com>
2025-05-22 22:58:45 -0400

    use hash router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { BeMine } from "./pages/BeMine/BeMine";
 import { GirlfriendDay2024 } from "./pages/GirlfriendDay2024/GirlfriendDay2024";
 import { GrievancePortal } from "./pages/GrievancePortal/GrievancePortal";
@@ -11,32 +11,18 @@ import theme from "./theme";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement!);
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/be-mine",
-    element: <BeMine />,
-  },
-  {
-    path: "/girlfriend-day-2024",
-    element: <GirlfriendDay2024 />,
-  },
-  {
-    path: "/grievance-portal",
-    element: <GrievancePortal />,
-  },
-  {
-    path: "/*",
-    element: <Home />,
-  },
-]);
 
 root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/be-mine" element={<BeMine />} />
+        <Route path="/girlfriend-day-2024" element={<GirlfriendDay2024 />} />
+        <Route path="/grievance-portal" element={<GrievancePortal />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </HashRouter>
   </ThemeProvider>
 );
